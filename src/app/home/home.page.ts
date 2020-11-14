42f3ca4fdee37dc3b1949e406e5ca22d21845d0c
Alvaro Cantariño <alvaro.cj05@gmail.com>
2020-11-14 12:32:56 +0100

    Testing Toasts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {AuthenticationService} from '../services/authentication.service';
 import {Router} from '@angular/router';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,7 @@ export class HomePage {
   userEmail = '';
   userPassword: any;
 
-  constructor(private authService: AuthenticationService, private router: Router) {
+  constructor(private authService: AuthenticationService, private router: Router, private toastController: ToastController) {
 
   }
 
@@ -21,8 +22,18 @@ export class HomePage {
         .then(res => {
           console.log('alvaro' + res);
           this.router.navigate(['/main']).then(r => {console.log('entraste'); }, error => {console.log('no entraste'); });
+          this.presentToast('Inicio correcto');
         }, error => {
           console.log('alvaro' + error);
+          this.presentToast(error);
         });
   }
+
+    async presentToast(info) {
+        const toast = await this.toastController.create({
+            message: info,
+            duration: 2000
+        });
+        await toast.present();
+    }
 }
